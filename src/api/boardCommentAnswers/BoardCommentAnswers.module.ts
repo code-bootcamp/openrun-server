3343f211258b5fbd0174aaf7ad124115bba6a025
uiop5487 <uiop5487@gmail.com>
2022-08-30 23:50:58 +0900

    docker-compose setting

---
+++ src/api/boardCommentAnswers/BoardCommentAnswers.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { BoardCommentAnswersResolver } from './BoardCommentAnswers.resolver';
+import { BoardCommentAnswersService } from './BoardCommentAnswers.service';
+
+@Module({
+  providers: [
+    BoardCommentAnswersResolver, //
+    BoardCommentAnswersService,
+  ],
+})
+export class BoardCommentAnswersModule {}
