de584349ef99cc62a91338e870f7ab71b6dce630
uiop5487 <uiop5487@gmail.com>
2022-09-02 00:50:06 +0900

    [FEAT] addInterestList API 생성(#37)

--- src/api/interests/interests.service.ts
+++ src/api/interests/interests.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Interest } from './entities/interests.entity';
 
 @Injectable()
-export class InterestsService {}
+export class InterestsService {
+  constructor(
+    @InjectRepository(Interest)
+    private readonly interestRepository: Repository<Interest>, //
+  ) {}
+
+  create({ user, board }) {
+    return this.interestRepository.save({
+      user,
+      board,
+    });
+  }
+}
