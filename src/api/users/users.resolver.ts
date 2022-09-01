899f8f3a4b14c22322db5ae2f0482042f325a12e
Annie-Cho <ani100482@gmail.com>
2022-09-01 15:33:55 +0900

    [MODIFIED] createUser에 데이터 존재유무 판단 로직 추가(#53)

--- src/api/users/users.resolver.ts
+++ src/api/users/users.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateUserInput } from './dto/createUser.input';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
 
 @Resolver()
 export class UsersResolver {
@@ -15,9 +16,24 @@ export class UsersResolver {
   }
 
   @Mutation(() => User)
-  createUser(
+  async createUser(
     @Args('createUserInput') createUserInput: CreateUserInput, //
   ) {
-    return this.usersService.create({ createUserInput });
+    //유저가 존재하는지 확인
+    await this.usersService.checkIsUserAvailable({
+      email: createUserInput.email,
+    });
+
+    //닉네임이 존재하는지 확인
+    await this.usersService.checkIsNickNameAvailable({
+      nickName: createUserInput.nickName,
+    });
+
+    //패스워드 Encrypt
+    const { password, cardInfoInput, ...user } = createUserInput;
+    const hashedPwd = await bcrypt.hash(password, 10);
+
+    //유저 생성
+    return this.usersService.create({ _user: user, hashedPwd, cardInfoInput });
   }
 }
--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CardInfosService } from '../cardInfos/cardInfos.service';
@@ -20,9 +20,7 @@ export class UsersService {
     });
   }
 
-  async create({ createUserInput }) {
-    const { cardInfoInput, ...userInfo } = createUserInput;
-
+  async create({ _user, hashedPwd: password, cardInfoInput }) {
     //CardInfo저장
     const cardInfoResult = await this.cardInfosService.create({
       ...cardInfoInput,
@@ -30,12 +28,27 @@ export class UsersService {
 
     //User 데이터 저장
     const userResult = await this.userRepository.save({
-      ...userInfo,
+      ..._user,
+      password,
       point: 0,
       rating: 0,
       report: 0,
       cardInfo: cardInfoResult,
     });
     return userResult;
+  }
+
+  async checkIsUserAvailable({ email }) {
+    const result = await this.userRepository.findOne({ where: { email } });
+    if (result) {
+      throw new UnprocessableEntityException('이미 사용중인 아이디입니다.');
+    }
+  }
+
+  async checkIsNickNameAvailable({ nickName }) {
+    const result = await this.userRepository.findOne({ where: { nickName } });
+    if (result) {
+      throw new UnprocessableEntityException('이미 사용중인 닉네임입니다.');
+    }
   }
 }
