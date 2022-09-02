89e85021ba04a1e3e00586e9314efbddbdd9cd2c
uiop5487 <uiop5487@gmail.com>
2022-09-02 16:37:25 +0900

    [FEAT] fetchPaymentHistory API(#26)

--- src/api/paymentHistories/paymentHistories.module.ts
+++ src/api/paymentHistories/paymentHistories.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CardInfosService } from '../cardInfos/cardInfos.service';
+import { CardInfo } from '../cardInfos/entities/cardInfo.entity';
+import { User } from '../users/entities/user.entity';
+import { UsersService } from '../users/users.service';
+import { PaymentHistory } from './entities/paymentHistory.entity';
 import { PaymentHistoriesResolver } from './paymentHistories.resolver';
 import { PaymentHistoriesService } from './paymentHistories.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      PaymentHistory, //
+      User,
+      CardInfo,
+    ]),
+  ],
   providers: [
     PaymentHistoriesResolver, //
     PaymentHistoriesService,
+    UsersService,
+    CardInfosService,
   ],
 })
 export class PaymentHistoriesModule {}
--- src/api/paymentHistories/paymentHistories.service.ts
+++ src/api/paymentHistories/paymentHistories.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PaymentHistory } from './entities/paymentHistory.entity';
 
 @Injectable()
-export class PaymentHistoriesService {}
+export class PaymentHistoriesService {
+  constructor(
+    @InjectRepository(PaymentHistory)
+    private readonly paymentHistoryRepository: Repository<PaymentHistory>,
+  ) {}
+
+  findAllByUser({ user }) {
+    return this.paymentHistoryRepository.find({
+      where: { user: { id: user.id } },
+    });
+  }
+}
