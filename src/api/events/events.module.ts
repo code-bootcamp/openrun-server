64157a3b066720158c01abc262a03e27a5c21a5f
uiop5487 <uiop5487@gmail.com>
2022-09-05 03:29:26 +0900

    [ADD] fetchEvents API 검색기능 추가

--- src/api/events/events.module.ts
+++ src/api/events/events.module.ts
@@ -3,12 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { EventsResolver } from './events.resolver';
 import { EventsService } from './events.service';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Event, //
     ]),
+    ElasticsearchModule.register({
+      node: 'http://elasticsearch:9200', //
+    }),
   ],
   providers: [
     EventsResolver, //
