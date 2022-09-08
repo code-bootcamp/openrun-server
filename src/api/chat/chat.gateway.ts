bf8d04a542f3c6a5834697648dae2d57202fa1d6
uiop5487 <uiop5487@gmail.com>
2022-09-08 19:13:35 +0900

    [FEAT] fetchUserChatList API 생성(#172)

--- src/api/chat/chat.gateway.ts
+++ src/api/chat/chat.gateway.ts
@@ -8,6 +8,7 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { Repository } from 'typeorm';
+import { BoardsService } from '../boards/boards.service';
 import { User } from '../users/entities/user.entity';
 import { ChatService } from './chat.service';
 import { ChatMessage } from './entities/chatMessage.entity';
@@ -26,6 +27,8 @@ export class ChatGateway {
     private readonly usersRepository: Repository<User>,
 
     private readonly chatService: ChatService,
+
+    private readonly boardsService: BoardsService,
   ) {}
   @WebSocketServer()
   server: Server;
@@ -33,39 +36,111 @@ export class ChatGateway {
   wsClients = [];
 
   @SubscribeMessage('message')
-  connectSomeone(@MessageBody() data: string, @ConnectedSocket() client) {
-    const [nickname, room] = data;
-    console.log(data);
-    console.log(`${nickname}님이 코드: ${room}방에 접속했습니다.`);
-    const comeOn = `${nickname}님이 입장했습니다.`;
-    this.server.emit('comeOn' + room, comeOn);
+  async connectSomeone(@MessageBody() data: string, @ConnectedSocket() client) {
+    const [nickName, runnerId, boardId] = data;
+
+    const findChatRoom = await this.chatService.findOne({
+      boardId,
+    });
+
+    const host = await this.usersRepository.findOne({
+      where: { nickName },
+    });
+
+    const runner = await this.usersRepository.findOne({
+      where: { id: runnerId },
+    });
+
+    const board = await this.boardsService.findOne({
+      boardId,
+    });
+
+    let room;
+
+    if (!findChatRoom) {
+      room = await this.chatService.create({
+        room: 'first' + boardId,
+        host,
+        runner,
+        board,
+      });
+    } else {
+      room = findChatRoom;
+    }
+
+    console.log(room.room);
+
+    const findSellerMessage = await this.chatMessageRepository.findOne({
+      relations: {
+        room: true,
+        user: true,
+      },
+      where: { room: { room: room.room }, user: { nickName } },
+    });
+
+    const findRunnerMessage = await this.chatMessageRepository.findOne({
+      relations: {
+        room: true,
+        user: true,
+      },
+      where: { room: { room: room.room }, user: { id: runnerId } },
+    });
+
+    const comeOn = `${nickName}님이 입장했습니다.`;
+
+    if (!findSellerMessage) {
+      this.chatMessageRepository.save({
+        message: comeOn,
+        room,
+        user: host,
+      });
+
+      this.server.emit('first' + boardId, [comeOn, nickName]);
+    } else if (!findRunnerMessage) {
+      this.chatMessageRepository.save({
+        message: comeOn,
+        room,
+        user: runner,
+      });
+
+      this.server.emit('first' + boardId, [comeOn, nickName]);
+    }
+
+    console.log(`${nickName}님이 코드: ${room.room}방에 접속했습니다.`);
+
     this.wsClients.push(client);
   }
 
-  private broadcast(event, client, message: any) {
-    for (const c of this.wsClients) {
-      if (client.id == c.id) continue;
-      c.emit(event, message);
-    }
-  }
+  // private broadcast(event, client, message: any) {
+  //   console.log(event);
+  //   console.log('=================', this.wsClients);
+  //   for (const c of this.wsClients) {
+  //     if (client.id == c.id) continue;
+  //     console.log(event);
+  //     c.emit(event, message);
+  //   }
+  // }
 
   @SubscribeMessage('send')
   async sendMessage(@MessageBody() data: string, @ConnectedSocket() client) {
     const [room, nickname, message] = data;
-    console.log(data);
+
     const user = await this.usersRepository.findOne({
       where: { nickName: nickname },
     });
 
-    // this.chatService.findOne()
+    const findRoom = await this.chatRoomRepository.findOne({
+      where: { room: room },
+    });
 
-    console.log(room);
     await this.chatMessageRepository.save({
       message: message,
-      room: { room },
+      room: findRoom,
       user: user,
     });
-    console.log(`${client.id} : ${data}`);
-    this.broadcast(room, client, [nickname, message]);
+
+    this.server.emit(room, [nickname, message]);
+
+    // this.broadcast(room, client, [nickname, message]);
   }
 }
