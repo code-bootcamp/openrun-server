942adadbc88190c9dae93cc41e045726eb244b70
EriicHaan12 <han33836914@gmail.com>
2022-09-18 14:47:40 +0900

    [FEAT] refreshes 폴더 생성, refreshes.service.ts 에서 dueDate 가 today 시간 넘어가게 될 경우 , status 변경 및 신청금액 되돌려주기 API 기능 추가 (#326)

---
+++ src/api/refreshes/refreshes.module.ts
@@ -0,0 +1,9 @@
+// import { Module } from '@nestjs/common';
+// import { ScheduleModule } from '@nestjs/schedule';
+// import { RefreshesService } from './refreshes.service';
+
+// @Module({
+//   imports: [ScheduleModule.forRoot()],
+//   providers: [RefreshesService],
+// })
+// export class RefreshesModule {}
