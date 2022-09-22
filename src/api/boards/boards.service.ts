694500ad2790ccfca9234baa697d1632e9f96d52
Annie-Cho <ani100482@gmail.com>
2022-09-22 16:15:31 +0900

    [MODIFY] error 처리 변경(#403)

--- src/api/boards/boards.service.ts
+++ src/api/boards/boards.service.ts
@@ -133,6 +133,7 @@ export class BoardsService {
       };
     } catch (error) {
       await queryRunner.rollbackTransaction();
+      throw new UnprocessableEntityException(error);
     } finally {
       await queryRunner.release();
     }
